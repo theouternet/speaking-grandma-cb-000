d0041784db5a6ca499c18b6fe7de525b126dd677
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-08-22 17:55:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 def speak_to_grandma(string)
   if string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
-    elsif string == string.upcase
-    return "NO, NOT SINCE 1938!"
     elsif string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+    elsif string == string.upcase
+    return "NO, NOT SINCE 1938!"
   end
 end
